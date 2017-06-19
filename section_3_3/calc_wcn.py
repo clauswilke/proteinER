a2233ab16aae3420796ba60e6a3eeb3594516f37
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-06-19 14:09:51 -0500

    added doc strings to calc_wcn.py

--- section_3_3/calc_wcn.py
+++ section_3_3/calc_wcn.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
-import sys
+
+'''
+This script parses an input PDB file and returns weighted contact number (WCN)
+values, calculated with respect to the alpha-carbon (wcn_ca) and the sidechain
+center-of-mass (wcn_sc).
+
+Author: Benjamin R. Jack
+'''
+
 import os
 import csv
 import warnings
@@ -14,16 +22,16 @@ RESDICT = {'ALA': 'A', 'CYS': 'C', 'ASP': 'D', 'GLU': 'E', 'PHE': 'F', \
 
 def inv_sq_distance(coord1, coord2):
     '''
-    Doc string
+    Returns the inverse squared distance between any two coordinates.
     '''
     distance = 0.0
-    for i in range(len(coord1)):
-        distance += (coord1[i]-coord2[i])**2
+    for i, j in zip(coord1, coord2):
+        distance += (i-j)**2
     return 1/distance
 
 def calculate_wcn(residues):
     '''
-    Doc string
+    Calculates weighted contact number (WCN).
     '''
     for residue in residues:
         wcn_ca = 0
@@ -39,24 +47,29 @@ def calculate_wcn(residues):
 
     return residues
 
-def process_residue(residue, structure):
+def process_residue(residue):
     '''
-    Doc string
+    Processes a single residue to determine the coordinates of the alpha-carbon
+    and the sidechain center-of-mass. Also checks for missing atoms in a
+    residue.
     '''
     output_dict = {}
     atoms_seen = []
+    # Convert three letter amino acid to one letter
     output_dict['amino_acid'] = RESDICT[residue.resname]
-    output_dict['residue'] = residue.get_id()[1]
+    # Grab residue number AND any insertion site labeling (11A, 11B, etc.)
+    output_dict['residue'] = str(residue.get_id()[1]) + \
+                             residue.get_id()[2].strip()
     output_dict['chain'] = residue.get_full_id()[2]
+    # Coordinates of all sidechain atoms in this residue
     sidechain_coords = []
-    for atom in structure.get_atoms():
-        # atom.name is equivalent to atom.get_id()
-        if atom.parent.id != residue.id:
-            continue
+    for atom in residue:
         atoms_seen.append(atom.name)
         if atom.name == 'CA':
+            # Save alpha-carbon coordinates
             output_dict['coord_ca'] = atom.get_coord()
         if atom.name not in ['C', 'CA', 'O', 'N']:
+            # Must be a sidechain atom...
             sidechain_coords.append(atom.get_coord())
 
     warning_message = "Missing {} in residue (" + \
@@ -64,29 +77,33 @@ def process_residue(residue, structure):
                         str(output_dict['amino_acid']) + ")"
 
     for mainchain_atom in ['N', 'C', 'O']:
+        # Warn about any missing mainchain atoms
         if mainchain_atom not in atoms_seen:
             warnings.warn(warning_message.format(mainchain_atom),
                           RuntimeWarning)
     if 'coord_ca' not in output_dict:
+        # Cannot calculate WCN without at least alpha-carbon
         raise RuntimeError(warning_message.format('CA') +
                            '. Cannot calculate C-alpha WCN.')
 
     if len(sidechain_coords) == 0:
+        # Warn about missing sidechain for amino acids other than glycine
         if output_dict['amino_acid'] != 'G':
             warnings.warn(warning_message.format('sidechain') +
                           '. Using CA instead.', RuntimeWarning)
         sidechain_coords.append(output_dict['coord_ca'])
 
-    # Calculate side chain properties:
-    output_dict['sidechain_size'] = len(sidechain_coords)
+    # Calculate side chain center of mass
     output_dict['sidechain_center'] = sum(sidechain_coords)/\
-                                        output_dict['sidechain_size']
+                                      len(sidechain_coords)
 
     return output_dict
 
 def collect_coordinates(structure):
     '''
-    Doc string
+    Loops over all residues in a structure and collects coordinates for alpha-
+    carbons and sidechain center-of-mass. Returns a list of dictionaries, where
+    each dictionary corresponds to residue in the structure.
     '''
     output_list = []
     for residue in structure.get_residues():
@@ -94,12 +111,13 @@ def collect_coordinates(structure):
         if het_flag[0:2] == 'H_' or het_flag[0] == 'W':
             # Skip hetatoms and waters in PDB
             continue
-        output_list.append(process_residue(residue, structure))
+        output_list.append(process_residue(residue))
     return output_list
 
 def main():
     '''
-    Doc string
+    Parse an input PDB file and return a CSV with weighted contact number
+    values.
     '''
     parser = argparse.ArgumentParser(
         description='Calculate WCN values for an input PDB.')
@@ -114,12 +132,14 @@ def main():
         # If no output prefix given, assign prefix using input filename
         args.o = pdb_name
     output_wcn = args.o + '.wcn.csv'
-
+    # Load in PDB with BioPython
     pdb_parser = PDBParser()
     structure = pdb_parser.get_structure(pdb_name.upper(), args.pdb)
+    # Collect coordinate information
     output_list = collect_coordinates(structure)
+    # Calculate WCN from coordinates
     output_list = calculate_wcn(output_list)
-
+    # Write output to a CSV
     with open(output_wcn, 'w') as csvfile:
         writer = csv.DictWriter(csvfile,
                                 fieldnames=['residue', 'chain', 'amino_acid',
