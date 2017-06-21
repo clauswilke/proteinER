5a5bca5fd76d71ccaef0c0d2656213daf84bd7d0
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2017-06-21 17:31:37 -0500

    Adding scripts and input/output files for secion 3.2 measuring site-wise dN/dS

---
+++ src/find_conserved_sites.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+'''
+This script fixes HyPhy's FEL1 site-wise dN/dS values. The script finds convserved sites in a sequence alignment and assigns these sites dN/dS of 0.  
+
+Author: Dariya K. Sydykova
+'''
+
+import argparse
+from Bio import AlignIO
+
+def find_unchanged_sites(aln, rates_file, out_file):
+
+	out=open(out_file,"w")
+	out.write("site,rate\n")
+	
+	total_col=len(aln[0]) #total number of sites
+	
+	for i in range(total_col):
+		#a list of all amino acids at site i
+		col = aln[:,i]
+		
+		#checks if the list of amino acids at site i is identical to 
+		#the first amino acid in a list repeated the number of times the list is
+		if col == len(col) * col[0]:
+			line=str(i+1)+',0\n' 
+		else:
+			line=str(i+1)+'\tFALSE\n' 
+		out.write(line)
+
+def main():
+	'''
+	find conserved sites in FASTA file and assign dN/dS of 0 to those sites.
+	'''
+	#creating a parser
+	parser = argparse.ArgumentParser(description='Assign dN/dS of 0 to conserved sites.')
+	#adding arguments 
+	parser.add_argument('-a', metavar='<aa_aln.fasta>', type=str,
+                    help='input amino acid alignment file')
+    parser.add_argument('-r', metavar='<rates.csv>', type=str,
+                    help='HyPhy FEL1 file')
+	parser.add_argument('-o', metavar='<processed_rates.csv>', type=str,
+                    help='output processed rates file')
+
+	args = parser.parse_args()
+
+	#set up output file name if none is given
+	if args.o is None:
+		out_rates = "processed_"+args.r
+	else:
+		out_rates = args.o
+		
+	aln_file=args.a
+	rates_file=args.r
+
+	aln = AlignIO.read(aln_file, "fasta") 
+	find_unchanged_sites(aln, rates_file, out_rates)
+
+if __name__ == "__main__":
+	main()
